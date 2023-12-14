b7882cbe2b228dfa1889e7b053d12c020d8e087c
webshoposcar1164 <cheaptrip1164@gmail.com>
2023-12-15 06:27:04 +0900

    投稿ページ作成

---
+++ src/components/CreatePost.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./CreatePost.css";
+
+const CreatePost = () => {
+	const [title, setTitle] = useState();
+	const [text, setText] = useState();
+
+	const createPost = () => {
+		console.log(title);
+		console.log(text);
+	};
+
+	return (
+		<div className="createPostPage">
+			<div className="postContainer">
+				<h1>記事を投稿する</h1>
+				<div className="inputPost">
+					<div>タイトル</div>
+					<input
+						type="text"
+						placeholder="タイトルを記入"
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</div>
+				<div className="inputPost">
+					<div>投稿</div>
+					<textarea
+						placeholder="投稿内容を記入"
+						onChange={(e) => setText(e.target.value)}
+					></textarea>
+				</div>
+				<button className="postButton" onClick={createPost}>
+					投稿する
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default CreatePost;
