0ee3eba640cbf4ba6a961018ffbebdd0182d723d
webshoposcar1164 <cheaptrip1164@gmail.com>
2023-12-17 20:52:21 +0900

    ホーム画面作成・firebaseから投稿取得

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+import "./Home.css";
 
 const Home = () => {
-	return <div>Home</div>;
+	const [postList, setPostList] = useState([]);
+	useEffect(() => {
+		const getPosts = async () => {
+			const data = await getDocs(collection(db, "posts"));
+			// console.log(data);
+			// console.log(data.docs);
+			// console.log(data.docs.map((doc) => ({ doc })));
+			// console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+			setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		};
+		getPosts();
+		// console.log(postList);
+	}, []);
+	return (
+		<div className="homePage">
+			{postList.map((post) => {
+				return (
+					<div className="postContents" key={post.id}>
+						<div className="postHeader">
+							<h1>{post.title}</h1>
+						</div>
+						<div className="postTextContainer">{post.postText}</div>
+						<div className="nameAndDeleteButton">
+							<h3>@{post.author.username}</h3>
+							<button>削除</button>
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Home;
